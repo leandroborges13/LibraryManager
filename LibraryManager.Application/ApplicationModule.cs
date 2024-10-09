2b49665b29031ba5e01fa28ca8e9b66d52ec3a30
Leandro Souza Lima Borges <leandroborges87@yahoo.com.br>
2024-10-09 11:30:12 -0300

    Validações Fluent Validation.

--- LibraryManager.Application/ApplicationModule.cs
+++ LibraryManager.Application/ApplicationModule.cs
@@ -1,4 +1,7 @@
-﻿using LibraryManager.Application.Commands.LoanCommands.InsertLoan;
+﻿using FluentValidation;
+using FluentValidation.AspNetCore;
+using LibraryManager.Application.Commands.LoanCommands.InsertLoan;
+using LibraryManager.Application.Commands.UserCommands.InsertUser;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace LibraryManager.Application
@@ -8,7 +11,8 @@ namespace LibraryManager.Application
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
             services
-                .AddHandlers();
+                .AddHandlers()
+                .AddValidation();
 
             return services;
         }
@@ -17,6 +21,13 @@ namespace LibraryManager.Application
         {
             services.AddMediatR(config => config.RegisterServicesFromAssemblyContaining<InsertLoanCommand>());
 
+            return services;
+        }
+
+        private static IServiceCollection AddValidation(this IServiceCollection services)
+        {
+            services.AddFluentValidationAutoValidation()
+                .AddValidatorsFromAssemblyContaining<InsertUserCommand>();
             return services;
         }
     }
