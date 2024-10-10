6bd2c8d63db623d7637e76b81794543cae1a4819
Leandro Souza Lima Borges <leandroborges87@yahoo.com.br>
2024-10-09 22:51:31 -0300

    validações finais.

---
+++ LibraryManager.Application/Validators/LoanValidators/InsertLoanValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using LibraryManager.Application.Commands.BookCommands.InsertBook;
+using LibraryManager.Application.Commands.LoanCommands.InsertLoan;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManager.Application.Validators.LoanValidators
+{
+    public class InsertLoanValidator : AbstractValidator<InsertLoanCommand>
+    {
+        public InsertLoanValidator() {
+            RuleFor(p => p.DaysLoan)
+                .GreaterThan(0).WithMessage("Dias de empréstimo tem que ser maior que zero.");
+        }
+    }
+}
